2c13cc5ede59af0c1c1422c8a413c32bb9cb0e9a
arogya <arogyabichpuria16@gmail.com>
2021-07-22 11:21:52 +0530

    implemented basic features

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const socket = io("/");
+
+let myVideoStream;
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const videoGrid = document.getElementById("video-grid");
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+
+navigator.mediaDevices
+.getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
+
+// socket.emit('join-room',ROOM_ID);
+
+socket.on("user-connected", (userId) => {
+   
+  connectToNewUser(userId);
+});
+
+const connectToNewUser = (userId) => {
+  console.log(`${userId} Entered the room`);
+};
+
+
+peer.on('open', function(id){
+    console.log('hey');
+}
+);
+
+
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const app= express();
+const server=require('http').Server(app);
+const io=require('socket.io')(server);
+const {ExpressPeerServer} = require('peer');
+
+const peerServer = ExpressPeerServer(server,{
+  debug:true
+});
+
+const {v4:uuidv4 } = require('uuid');
+
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidv4()}`); 
+})
+
+app.get('/:room',(req,res)=>{
+  res.render('room', {roomId: req.params.room})
+});
+
+
+server.listen(3030,(req,res)=>{
+    console.log('connected to port 3030');
+});
+
+io.on('connection',socket =>{
+  socket.on('join-room',(roomId,userId)=>{
+    console.log(roomId,userId);
+   socket.join(roomId);
+   socket.to(roomId).emit('user-connected',userId);
+  }) 
+})
